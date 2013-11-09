f0dd3dbe7eaecce9a1cf572250708d44c32dc251
Sean Kelley <skelley@palantir.com>
2013-11-09 02:02:19 -0800

    Tweaking theme.

--- antigen-git.zsh
+++ antigen-git.zsh
@@ -4,3 +4,8 @@ alias gf='git fetch'
 compdef _git gf=git-fetch
 alias grb='git rebase'
 compdef _git gst=git-rebase
+alias gsh='git show'
+compdef _git gst=git-show
+# clobber existing gc! because I prefer this functionality
+alias gc!='git commit -v --amend -C HEAD'
+compdef _git gc!=git-commit
--- antigen-theme.zsh
+++ antigen-theme.zsh
@@ -1,9 +1,9 @@
 antigen theme simple
 
-PROMPT='%{$fg[yellow]%}%~%{$fg_bold[blue]%}
-$(git_prompt_info)%{$reset_color%} ⟩ '
+PROMPT='%{$fg[yellow]%}%~
+%{$fg[blue]%}$(git_prompt_info)%{$reset_color%}⟩ '
 
 ZSH_THEME_GIT_PROMPT_PREFIX=""
-ZSH_THEME_GIT_PROMPT_SUFFIX=""
-ZSH_THEME_GIT_PROMPT_DIRTY=" ×"
-ZSH_THEME_GIT_PROMPT_CLEAN=""
+ZSH_THEME_GIT_PROMPT_SUFFIX=" "
+ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg[red]%}!"
+ZSH_THEME_GIT_PROMPT_CLEAN=" %{$fg[green]%}-"
