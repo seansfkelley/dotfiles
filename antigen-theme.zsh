6c91a2f30f83c38290d08a1311abb6ad96ded2c2
Sean Kelley <skelley@palantir.com>
2013-11-11 10:27:26 -0800

    Adding option to disable the dirty notifier in the git prompt.

--- antigen-theme.zsh
+++ antigen-theme.zsh
@@ -1,7 +1,25 @@
 antigen theme simple
 
+declare ENABLE_DIRTY_GIT_PROMPT=true
+
+# Stolen and adapted from
+# https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/git.zsh
+function git_prompt_info_no_dirty() {
+  ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(command git rev-parse --short HEAD 2> /dev/null) || return
+  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$ZSH_THEME_GIT_PROMPT_SUFFIX"
+}
+
+function get_git_prompt_info() {
+  if ( test $ENABLE_DIRTY_GIT_PROMPT = true ); then
+    git_prompt_info
+  else
+    git_prompt_info_no_dirty
+  fi
+}
+
 PROMPT='%{$fg[yellow]%}%~
-%{$fg[blue]%}$(git_prompt_info)%{$reset_color%}⟩ '
+%{$fg[blue]%}$(get_git_prompt_info)%{$reset_color%}⟩ '
 
 ZSH_THEME_GIT_PROMPT_PREFIX=""
 ZSH_THEME_GIT_PROMPT_SUFFIX=" "
