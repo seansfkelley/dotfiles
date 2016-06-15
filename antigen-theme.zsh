ef084dcfc9406de4708fecaa20b5f05705304dd5
Sean Kelley <skelley@palantir.com>
2016-06-15 15:34:39 -0700

    Fix missing local.

--- antigen-theme.zsh
+++ antigen-theme.zsh
@@ -5,6 +5,7 @@ declare ENABLE_DIRTY_GIT_PROMPT=true
 # Stolen and adapted from
 # https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/git.zsh
 function git_prompt_info_no_dirty() {
+  local ref
   ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
   ref=$(command git rev-parse --short HEAD 2> /dev/null) || return
   echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$ZSH_THEME_GIT_PROMPT_SUFFIX"
