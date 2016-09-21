068d3af9765293cfc0c972b6a1245ff6da4455ce
Sean Kelley <skelley@palantir.com>
2016-09-21 16:15:54 -0700

    Stuff.

--- other-commands.zsh
+++ other-commands.zsh
@@ -1,6 +1,14 @@
 alias t='t_time_tracker'
 alias b='afplay /System/Library/Sounds/Glass.aiff -v 2'
 alias n='osascript -e "display notification with title \"Task Finished\""'
-alias f='find . -iname '
 alias jsonpretty='python -m json.tool'
 alias gimme-usrlocal='sudo chown -R skelley /usr/local/{lib,bin,share}'
+alias fuck-carbonblack='ps aux | grep CbOsxSensorService | grep CarbonBlack | sed -E "s/ +/ /g" | cut -d " " -f 2 | sudo xargs kill -9'
+
+function f() {
+  find . -iname "*$1*"
+}
+
+function npmtsd() {
+  npm "$@" && tsd "$@"
+}
