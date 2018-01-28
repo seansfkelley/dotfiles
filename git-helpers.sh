d7157e4e21c45712395abcc0d487f78f4afe3d03
Sean Kelley <seansfkelley@gmail.com>
2018-01-28 12:44:21 -0800

    Updatedate ghpr URL.

--- git-helpers.sh
+++ git-helpers.sh
@@ -37,6 +37,5 @@ export PATH="$GIT_HELPER_DIR/git-number:$PATH"
 
 alias gn='nocorrect git number --column'
 
-alias ghpr='git push origin HEAD -u && open https://github.palantir.build/$(git ls-remote --get-url origin | sed -E -e "s/^.+:(.+)\.git$/\1/")/compare/develop...$(git rev-parse --abbrev-ref HEAD)'
-
 alias gh='open $(git remote get-url origin | sed -Ee '"'"'s#(git@|git://)#https://#'"'"' -e '"'"'s@:([^:]+).git$@/\1@'"'"')'
+alias ghpr='git push origin HEAD -u && open https://github.com/$(git ls-remote --get-url origin | sed -E -e "s/^.+:(.+)\.git$/\1/")/compare/master...$(git rev-parse --abbrev-ref HEAD)'
