95938ac411475bbdb24fec1c82d4fe8bd88b917c
Sean Kelley <sean.kelley@carbonlighthouse.com>
2018-01-29 09:46:20 -0800

    Remove zsh bundles that I only use at work.

--- antigen-core.zsh
+++ antigen-core.zsh
@@ -3,11 +3,8 @@ source ~/.antigen.zsh
 antigen use oh-my-zsh
 
 antigen bundle command-not-found
-antigen bundle docker
 antigen bundle npm
-antigen bundle pip
-antigen bundle python
 antigen bundle tmuxinator
 antigen bundle yarn
-antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle git
+antigen bundle zsh-users/zsh-syntax-highlighting
