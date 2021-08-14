6c764a127726f3548940555562557b3b72c7226c
Sean Kelley <seansfkelley@gmail.com>
2021-08-14 15:27:03 -0400

    Update git aliases.

--- specifics/git.zsh
+++ specifics/git.zsh
@@ -3,10 +3,6 @@ alias gh='open $(git remote get-url origin | sed -Ee '"'"'s#(git@|git://)#https:
 alias ghpr='git push origin HEAD -u && open https://github.com/$(git ls-remote --get-url origin | sed -E -e "s/^.+:(.+)\.git$/\1/")/compare/master...$(git rev-parse --abbrev-ref HEAD)'
 alias githlog='git log --date-order --all --graph --format="%C(green)%H%Creset %C(yellow)%an%Creset %C(blue bold)%ad%Creset %C(red bold)%d%Creset%s"'
 
-alias grb='git rebase'
-compdef _git grb=git-rebase
-alias gsh='git show'
-compdef _git gsh=git-show
 alias gpn='git push origin $(git rev-parse --abbrev-ref HEAD) -u'
 compdef _git gpn=git-push
 # clobber existing gc! because I prefer this functionality
@@ -15,8 +11,8 @@ compdef _git gc!=git-commit
 # clobber existing gbda because this functionality is more reasonable and safer
 alias gbda='git branch --no-color --merged origin/master | command grep -vE "^(\*|\s*(master|develop|dev|main)\s*$)" | command xargs -n 1 git branch -d'
 alias gbd!='git branch -D'
-# clobber existing gap because I never use apply
-alias gap='git -c "interactive.diffFilter=less" add -p'
+# clobber existing gapa because the configured diffFilter doesn't work with it
+alias gapa='git -c "interactive.diffFilter=less" add -p'
 alias gcom='git fetch && git checkout origin/master'
 alias gmm='git fetch && git merge origin/master'
 alias grbm='git fetch && git rebase origin/master'
