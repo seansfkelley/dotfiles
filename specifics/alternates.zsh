4c1815a2fb5d8c0fa1f30441b25d97b91740f779
Sean Kelley <seansfkelley@gmail.com>
2021-10-24 16:17:54 +0100

    Stop using alternates that are not useful.

--- specifics/alternates.zsh
+++ specifics/alternates.zsh
@@ -24,22 +24,20 @@ function use_alternate() {
 
 alias cat='use_alternate cat bat'
 alias du='use_alternate du dust'
-alias tree='use_alternate tree broot'
-alias find='use_alternate find fd'
 alias cut='use_alternate cut choose'
 alias top='use_alternate top htop'
 alias ping='use_alternate ping gping'
 alias ps='use_alternate ps procs'
 
-function man() {
-  if [ $(command -v cheat) ]; then
-    read "answer?want to cheat instead? y/N "
-    if [[ "$answer" == "" ]] || [[ "$answer" =~ '^[Nn]$' ]]; then
-      /usr/bin/man "$@"
-    else
-      cheat "$@"
-    fi
-  else
-    /usr/bin/man "$@"
-  fi
-}
+# function man() {
+#   if [ $(command -v cheat) ]; then
+#     read "answer?want to cheat instead? y/N "
+#     if [[ "$answer" == "" ]] || [[ "$answer" =~ '^[Nn]$' ]]; then
+#       /usr/bin/man "$@"
+#     else
+#       cheat "$@"
+#     fi
+#   else
+#     /usr/bin/man "$@"
+#   fi
+# }
