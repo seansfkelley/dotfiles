11e645ff1d7f218ddb3eb2a2aaa7cc39c7f44206
Sean Kelley <seansfkelley@users.noreply.github.com>
2022-04-10 12:53:28 -0700

    Add a few missing bits.

--- zsh/antigen-bundles.zsh
+++ zsh/antigen-bundles.zsh
@@ -1,9 +1,10 @@
-antigen use oh-my-zsh
+antigen bundle andrewferrier/fzf-z
 
+# this has to come after, since `use` sets the default and we rely on it later for the theme and stuff
+antigen use oh-my-zsh
 antigen bundle command-not-found
 antigen bundle tmuxinator
 antigen bundle git
 antigen bundle osx
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle z
-antigen bundle fzf-z
