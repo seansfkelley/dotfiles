4789ed6e3d0b4caff0b56ae56a3df3ad11de2b12
Sean Kelley <seansfkelley@users.noreply.github.com>
2022-04-11 18:50:51 -0700

    Fix fzf to respect gitignores.

--- zsh/specifics/fzf.zsh
+++ zsh/specifics/fzf.zsh
@@ -1,3 +1,7 @@
 # These are sort of redundant with fzf's defaults, but ^R seems to not override shell default.
 bindkey '^R' fzf-history-widget
 bindkey '^T' fzf-file-widget
+
+# These must be `export`ed for reasons that escape me.
+export FZF_DEFAULT_COMMAND='rg --files --hidden || true'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
