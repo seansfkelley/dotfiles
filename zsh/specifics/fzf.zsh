8e32a0900cf1ee90452b534baeaa994ebd470e78
Sean Kelley <seansfkelley@users.noreply.github.com>
2023-05-08 13:18:15 +0200

    New VSCode features; improve tmux spiltting UX.

--- zsh/specifics/fzf.zsh
+++ zsh/specifics/fzf.zsh
@@ -1,6 +1,7 @@
 # These are sort of redundant with fzf's defaults, but ^R seems to not override shell default.
 bindkey '^R' fzf-history-widget
 bindkey '^T' fzf-file-widget
+# TODO: why does alt-C include things from node-modules?
 
 # These must be `export`ed for reasons that escape me.
 export FZF_DEFAULT_COMMAND='rg --files --hidden || true'
