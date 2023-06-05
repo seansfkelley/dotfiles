0c21a6d047edbdb5e32ecabdbd5d7942e646b263
Sean Kelley <seansfkelley@users.noreply.github.com>
2023-06-05 17:12:31 +0100

    Fix alt-c behavior of fzf.

--- zsh/specifics/fzf.zsh
+++ zsh/specifics/fzf.zsh
@@ -1,8 +1,8 @@
 # These are sort of redundant with fzf's defaults, but ^R seems to not override shell default.
 bindkey '^R' fzf-history-widget
 bindkey '^T' fzf-file-widget
-# TODO: why does alt-C include things from node-modules?
 
 # These must be `export`ed for reasons that escape me.
 export FZF_DEFAULT_COMMAND='rg --files --hidden || true'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd --type directory --hidden" # fd respects ignores; but we DO want to include non-ignored hiddens
