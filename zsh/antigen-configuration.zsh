3d6087fd7e216f2e99b14757565094f317ddd3ab
Sean Kelley <seansfkelley@users.noreply.github.com>
2023-08-16 14:30:34 -0700

    I guess the git bundle works properly now when in the right order...

--- zsh/antigen-configuration.zsh
+++ zsh/antigen-configuration.zsh
@@ -3,11 +3,7 @@ antigen bundle andrewferrier/fzf-z
 # this has to come after, since `use` sets the default and we rely on it later for the theme and stuff
 antigen use oh-my-zsh
 
-if [[ $(arch) != "arm64" ]]; then
-  # this only seems to work when it's first in the list on this computer?
-  antigen bundle git
-fi
-
+antigen bundle git
 antigen bundle command-not-found
 antigen bundle tmuxinator
 antigen bundle osx
@@ -16,8 +12,3 @@ antigen bundle z
 antigen theme simple
 
 antigen apply
-
-if [[ $(arch) == "arm64" ]]; then
-  # what the shit https://github.com/zsh-users/antigen/issues/676
-  antigen bundle git
-fi
