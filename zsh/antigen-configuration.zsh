35eaa6da5d5c7684d749d7d1fbf02afb019c3adf
Sean Kelley <seansfkelley@users.noreply.github.com>
2023-11-24 22:17:53 -0500

    Revert "I guess the git bundle works properly now when in the right order..."
    This reverts commit 3d6087fd7e216f2e99b14757565094f317ddd3ab.
    
    It doesn't actually work on the personal laptop for some reason.

--- zsh/antigen-configuration.zsh
+++ zsh/antigen-configuration.zsh
@@ -3,7 +3,11 @@ antigen bundle andrewferrier/fzf-z
 # this has to come after, since `use` sets the default and we rely on it later for the theme and stuff
 antigen use oh-my-zsh
 
-antigen bundle git
+if [[ $(arch) != "arm64" ]]; then
+  # this only seems to work when it's first in the list on this computer?
+  antigen bundle git
+fi
+
 antigen bundle command-not-found
 antigen bundle tmuxinator
 antigen bundle osx
@@ -12,3 +16,8 @@ antigen bundle z
 antigen theme simple
 
 antigen apply
+
+if [[ $(arch) == "arm64" ]]; then
+  # what the shit https://github.com/zsh-users/antigen/issues/676
+  antigen bundle git
+fi
